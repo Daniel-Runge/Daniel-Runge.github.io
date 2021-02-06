ecb6b092197edf88fdb812dd4cdb7579f4ebfd78
Daniel <dpet20@student.aau.dk>
2021-02-06 23:59:44 +0100

    Rudimentary bubble generation completed. Bubble removal on click issue fixed.

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,21 +1,20 @@
 function createBubble(){
   const body = document.querySelector('body');
   const createElement = document.createElement('div');
-  createElement.className = 'bubble';
   var size = 20 + Math.random() * 60;
+
+  createElement.className = 'bubble';
 
   createElement.style.width = size + 'px';
   createElement.style.height = size + 'px';
   createElement.style.left = Math.random() * 0.9*innerWidth + 'px';
+  createElement.style.cursor = 'pointer';
+  createElement.onclick = function() {createElement.remove();};
   body.appendChild(createElement);
 
   setTimeout(() => {
     createElement.remove()
-  }, 60000);
-}
-
-function popBubble(bubble) {
-  bubble.remove();
+  }, 25000);
 }
 
 setInterval(createBubble,5000);
